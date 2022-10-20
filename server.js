05afa6b3a5f567a8936589c4d4cf277d77a06ecd
Vincent Toon <vincenttoon22@gmail.com>
2022-10-19 21:42:48 -0500

    port change lol

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: process.env.Secret,
     cookie: {
-      expires: 1200000
+      expires: 120000
     },
     resave: false,
     saveUninitialized: true,
